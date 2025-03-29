be5609b05db7e47421a801b6eaca1d919afc498b
FedoRich-P <panchev_fedor@mail.ru>
2025-03-29 23:59:14 +0300

    Part-9. Sort with API.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,46 @@
+import { useState } from 'react';
 import { Categories } from '../components/Categories';
 import { Sort } from '../components/Sort';
-import { PizzaBlockSkeleton } from '../components/PizzaBlock/PizzaBlockSkeleton.tsx';
+import { PizzaBlockSkeleton } from '../components/PizzaBlock/PizzaBlockSkeleton';
 import { PizzaBlock } from '../components/PizzaBlock';
-import { useGetPizzas } from '../hooks/useGetPizzas.ts';
+import { useGetPizzas } from '../hooks/useGetPizzas';
 
+export const BASE_URL = 'https://67e65f996530dbd3110fb55d.mockapi.io/items';
 
 export const Home = () => {
-  const { pizza, isLoading, error } = useGetPizzas('https://67e65f996530dbd3110fb55d.mockapi.io/items');
+  const [categoryId, setCategoryId] = useState(0); // Просто number, без сложных типов
+  const [sortType, setSortType] = useState('rating');
+  const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('desc');
+
+  const { pizza, isLoading, error } = useGetPizzas({
+    url: BASE_URL,
+    category: categoryId,
+    sortBy: sortType,
+    order: sortDirection,
+  });
 
   if (error) return <h2>Ошибка: {error.message}</h2>;
+
   return (
-    <>
+    <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories categoryId={categoryId}
+                    setCategoryId={setCategoryId}
+        />
+        <Sort sortType={sortType}
+              setSortType={setSortType}
+              sortDirection={sortDirection}
+              setSortDirection={setSortDirection}
+        />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
         {isLoading
-          ? (Array.from({ length: 8 }).map((_, index) => <PizzaBlockSkeleton key={`skeleton-${index}`} />))
-          : (pizza.map(pizza => <PizzaBlock key={pizza.id} {...pizza} />)
-          )}
+          ? Array.from({ length: 8 }).map((_, index) => (
+            <PizzaBlockSkeleton key={`skeleton-${index}`} />
+          ))
+          : pizza.map(pizza => <PizzaBlock key={pizza.id} {...pizza} />)}
       </div>
-    </>
+    </div>
   );
 };
