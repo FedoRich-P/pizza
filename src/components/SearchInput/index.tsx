f30787308516017a324c9fc2fc85bbb968f43788
FedoRich-P <panchev_fedor@mail.ru>
2025-04-01 11:54:52 +0300

    Part-14. Redux / Refactoring

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,6 +1,6 @@
 import { FiSearch, FiX } from 'react-icons/fi';
 import styles from './SearchInput.module.scss';
-import { ChangeEvent, useEffect, useState, memo } from 'react';
+import { ChangeEvent, useEffect, useState, memo, useRef } from 'react';
 import { useDispatch} from 'react-redux';
 import { setSearchValue } from '../../features/filterSlice.ts';
 import { useDebounce } from '../../hooks/useDebounce.ts';
@@ -8,6 +8,8 @@ import { useDebounce } from '../../hooks/useDebounce.ts';
 export const SearchInput = memo(() => {
   const [localSearch, setLocalSearch] = useState('');
   const dispatch = useDispatch();
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const debouncedSearch = useDebounce(localSearch, 500);
 
@@ -22,11 +24,13 @@ export const SearchInput = memo(() => {
   function handleResetInputValue() {
     setLocalSearch('');
     dispatch(setSearchValue(''));
+    inputRef.current?.focus()
   }
 
   return (
     <div className={styles.container}>
       <input type="text"
+             ref={inputRef}
              value={localSearch}
              onChange={handleSearchChange}
              placeholder="Поиск пицц..."
