467ec09be5a78bd77b64996c986f6376bd061241
FedoRich-P <panchev_fedor@mail.ru>
2025-04-02 22:02:56 +0300

    Part-19. Redux /  Using a useParams / Editing a hook - usePizzaLogic

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,33 @@
 import './scss/app.scss';
-import { Header } from './components/Header';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '@/pages/Home';
 import { Cart } from '@/pages/Cart';
 import { NotFound } from '@/pages/NotFound';
 import { FullPizza } from '@/pages/FullPizza';
+import { MainLayout } from '@/layouts/MainLayout';
 
 
 function App() {
   return (
-    <>
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <div className="container">
-            <Routes>
-              <Route index element={<Home />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/pizza/:id" element={<FullPizza />} />
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<FullPizza />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
 export default App;
+
+// <div className="wrapper">
+//   <Header />
+//   <div className="content">
+//     <div className="container">
+//
+//     </div>
+//   </div>
+// </div>
 
